19c3127940eef3952dc32249e89ee4491bfae1ca
Prateek Rai <prateekrai7677@gmail.com>
2021-06-02 20:11:30 +0530

    Updates

--- static/js/main.d07882f7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpcandyshop=this.webpackJsonpcandyshop||[]).push([[0],{14:function(n,c,e){"use strict";e.r(c);e(0);var s=e(4),t=e.n(s),o=e(1),r=function(){return Object(o.jsx)("h1",{children:"build success"})};e(10).config(),t.a.render(Object(o.jsx)(r,{}),document.getElementById("root"))}},[[14,1,2]]]);
-//# sourceMappingURL=main.d07882f7.chunk.js.map
